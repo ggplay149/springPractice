ac7e612ac1a696eaf1e334dc5d92d1a25a35c698
ggplay149 <ggplay149@gmail.com>
2023-09-22 23:16:43 +0900

    check

---
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -0,0 +1,102 @@
+package com.studyolle.account;
+
+import com.studyolle.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.dom4j.rule.Mode;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.naming.AuthenticationException;
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+
+@Controller
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountService accountService;
+    private final AccountRepository accountRepository;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model) {
+        model.addAttribute(new SignUpForm());
+        return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        Account account = accountService.processNewAccount(signUpForm);
+        accountService.login(account);
+        return "redirect:/";
+    }
+
+    @GetMapping("/check-email-token")
+    public String checkEmailToken(String token, String email, Model model) {
+        Account account = accountRepository.findByEmail(email);
+        String view = "account/checked-email";
+        if (account == null) {
+            model.addAttribute("error", "wrong.email");
+            return view;
+        }
+
+        if (!account.isValidToken(token)) {
+            model.addAttribute("error", "wrong.token");
+            return view;
+        }
+
+        accountService.completeSignUp(account);
+
+        account.completeSignUp();
+        accountService.login(account);
+        model.addAttribute("numberOfUser", accountRepository.count());
+        model.addAttribute("nickname", account.getNickname());
+        return view;
+    }
+
+    @GetMapping("/check-email")
+    public String checkEmail(@CurrentUser Account account, Model model) {
+        model.addAttribute("email", account.getEmail());
+        return "account/check-email";
+    }
+
+    @GetMapping("/resend-confirm-email")
+    public String resendConfirmEmail(@CurrentUser Account account, Model model) {
+        if (!account.canSendConfirmEmail()) {
+            model.addAttribute("error", "인증 이메일은 1시간에 한번만 전송할 수 있습니다.");
+            model.addAttribute("email", account.getEmail());
+            return "account/check-email";
+        }
+
+        accountService.sendSignUpConfirmEmail(account);
+        return "redirect:/";
+    }
+
+    @GetMapping("/profile/{nickname}")
+    public String viewProfile(@PathVariable String nickname, Model model, @CurrentUser Account account){
+        Account byNickname = accountRepository.findByNickname(nickname);
+        if(nickname==null){
+            throw  new IllegalArgumentException(nickname+"에 해당하는 사용자가 없습니다.");
+        }
+
+        model.addAttribute(byNickname); //이름을 지정하지않으면 그 형태이름으로 감
+        model.addAttribute("isOwner",byNickname.equals(account));
+        return "account/profile";
+
+    }
+}
