71dbb44f92ef8d292f5a55a402250060fe69c23f
ggplay149 <ggplay149@gmail.com>
2023-09-26 23:32:21 +0900

    프로필 수정테스트

---
+++ src/test/java/com/studyolle/WithAccountSecurityContextFactory.java
@@ -0,0 +1,37 @@
+package com.studyolle;
+
+import com.studyolle.account.AccountService;
+import com.studyolle.account.SignUpForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+@RequiredArgsConstructor
+public class WithAccountSecurityContextFactory implements WithSecurityContextFactory<WithAccount> {
+
+    private final AccountService accountService;
+
+    @Override
+    public SecurityContext createSecurityContext(WithAccount withAccount) {
+        String nickname = withAccount.value();
+
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname(nickname);
+        signUpForm.setEmail(nickname+"@email.com");
+        signUpForm.setPassword("12345678");
+        accountService.processNewAccount(signUpForm);
+
+        UserDetails pricipal = accountService.loadUserByUsername(nickname);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(pricipal,pricipal.getPassword(),pricipal.getAuthorities());
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+
+
+        return context;
+
+    }
+}
