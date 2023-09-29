3f9cfdd17147c2366e0a5eaf4c65823e328ee720
ggplay149 <ggplay149@gmail.com>
2023-09-30 03:48:19 +0900

    알림기능

--- src/test/java/com/studyolle/settings/SettingsControllerTest.java
+++ src/test/java/com/studyolle/settings/SettingsControllerTest.java
@@ -3,13 +3,18 @@ package com.studyolle.settings;
 import com.studyolle.WithAccount;
 import com.studyolle.account.AccountRepository;
 import com.studyolle.account.AccountService;
+import com.studyolle.account.SignUpForm;
 import com.studyolle.domain.Account;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,61 +27,48 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class SettingsControllerTest {
 
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    AccountRepository accountRepository;
+    @Autowired MockMvc mockMvc;
+    @Autowired AccountRepository accountRepository;
+    @Autowired PasswordEncoder passwordEncoder;
 
     @AfterEach
-    void afterEach(){
+    void afterEach() {
         accountRepository.deleteAll();
     }
 
     @WithAccount("keesun")
     @DisplayName("프로필 수정 폼")
     @Test
-    void updateProfileForm() throws Exception{
-
-        String bio = "짧은 소개를 수정하는 경우";
+    void updateProfileForm() throws Exception {
         mockMvc.perform(get(SettingsController.SETTINGS_PROFILE_URL))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("account"))
                 .andExpect(model().attributeExists("profile"));
     }
 
-
     @WithAccount("keesun")
     @DisplayName("프로필 수정하기 - 입력값 정상")
     @Test
-    void updateProfile() throws Exception{
-
-        String bio = "짧은 소개를 수정하는 경우";
+    void updateProfile() throws Exception {
+        String bio = "짧은 소개를 수정하는 경우.";
         mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
-                .param("bio","짧은 소개를 수정하는 경우")
-                .with(csrf()))
+                        .param("bio", bio)
+                        .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl(SettingsController.SETTINGS_PROFILE_URL))
                 .andExpect(flash().attributeExists("message"));
 
         Account keesun = accountRepository.findByNickname("keesun");
-        assertEquals(bio,keesun.getBio());
+        assertEquals(bio, keesun.getBio());
     }
 
     @WithAccount("keesun")
-    @DisplayName("프로필 수정하기 - 입력값 에러(소개 너무 김)")
+    @DisplayName("프로필 수정하기 - 입력값 에러")
     @Test
-    void updateProfile_error() throws Exception{
-
-        String bio = "짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 " +
-                "경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우" +
-                "짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우" +
-                "짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우" +
-                "짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우" +
-                "짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우" +
-                "짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우짧은 소개를 수정하는 경우";
+    void updateProfile_error() throws Exception {
+        String bio = "길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 너무나도 길게 소개를 수정하는 경우. ";
         mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
-                        .param("bio",bio)
+                        .param("bio", bio)
                         .with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(view().name(SettingsController.SETTINGS_PROFILE_VIEW_NAME))
@@ -86,6 +78,47 @@ class SettingsControllerTest {
 
         Account keesun = accountRepository.findByNickname("keesun");
         assertNull(keesun.getBio());
+    }
+
+    @WithAccount("keesun")
+    @DisplayName("패스워드 수정 폼")
+    @Test
+    void updatePassword_form() throws Exception {
+        mockMvc.perform(get(SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordForm"));
+    }
+
+    @WithAccount("keesun")
+    @DisplayName("패스워드 수정 - 입력값 정상")
+    @Test
+    void updatePassword_success() throws Exception {
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_URL)
+                        .param("newPassword", "12345678")
+                        .param("newPasswordConfirm", "12345678")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(flash().attributeExists("message"));
+
+        Account keesun = accountRepository.findByNickname("keesun");
+        assertTrue(passwordEncoder.matches("12345678", keesun.getPassword()));
+    }
+
+    @WithAccount("keesun")
+    @DisplayName("패스워드 수정 - 입력값 에러 - 패스워드 불일치")
+    @Test
+    void updatePassword_fail() throws Exception {
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_URL)
+                        .param("newPassword", "12345678")
+                        .param("newPasswordConfirm", "11111111")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PASSWORD_VIEW_NAME))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("passwordForm"))
+                .andExpect(model().attributeExists("account"));
     }
 
 }
